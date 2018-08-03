d3346680b81fb58bc0cc2f2b3927ab129896ddc8
Gourav Johari <41551799+JohariGourav@users.noreply.github.com>
2018-08-03 09:12:50 -0400

    Add files via upload

---
+++ Streams/src/com/capgemini/streams/RandomNumbers.java
@@ -0,0 +1,14 @@
+package com.capgemini.streams;
+
+import java.util.Random;
+
+public class RandomNumbers {
+	
+	int random = Random.nextInt(49);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
